30e69dddfc743909850e28123b95469aeb3c0eeb
DESKTOP-U7R5T0Q\luxuan <523957947@qq.com>
2018-06-22 00:31:35 +0800

    换数据库，换框架，删除旧代码

--- lifesenceSystem/lifesenseSystem/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lifesenseSystem.MvcApplication" Language="C#" %>
--- lifesenceSystem/lifesenseSystem/JS/InPage/AddQQAD_Second.js
+++
@@ -1,6 +0,0 @@
-﻿$(function () {
-    $(".treeview").removeClass("active");
-    $(".treeview-menu").find("li").removeClass("active");
-    $("#treeview_2").addClass("active");
-    $("#treeview_2_1").addClass("active");
-})
