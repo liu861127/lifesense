f3ea7a608e2d2f6e6b3cfb3b8355cb2c0a6bac1e
DESKTOP-U7R5T0Q\luxuan <523957947@qq.com>
2018-06-25 23:29:18 +0800

    用户登录

---
+++ lifesense/BLL/http/CheckUser.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleLifesense;
+using lifesense.BLL.http.consts;
+using Maticsoft.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lifesense.BLL.http
+{
+ public   class CheckUser
+    {
+      public CheckUser()
+       {
+
+       }
+
+      public String getTempAuthorizeCode()
+      {
+          WebClient webClient = WebClient.instance;
+          try
+          {
+              String param = Consts.CHECK_USER + getParams();
+              String userInfo = webClient.GetHtml(param);
+              return userInfo;
+          }
+          catch (Exception ex)
+          {
+              return "";
+
+          }
+      }
+
+      private String getParams()
+      {
+          StringBuilder sb = new StringBuilder();
+          sb.Append("?username=lingxizhixia");
+          sb.Append("&password=jkeodmdk228H");
+          sb.Append("&tempAuthorizeCode=" + SyncDataManager.token);
+          return sb.ToString();
+      }
+
+    }
+}
