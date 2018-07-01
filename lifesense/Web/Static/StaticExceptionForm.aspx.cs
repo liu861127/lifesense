afbeccba6410b6380f6888b697191c3ed8939782
DESKTOP-U7R5T0Q\luxuan <523957947@qq.com>
2018-07-01 22:38:52 +0800

    代码同步

---
+++ lifesense/Web/Static/StaticExceptionForm.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lifesense.Web.Static
+{
+    public partial class StaticExceptionForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSysData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnShow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void chkIsAll_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
