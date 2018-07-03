1839f8e5aa21b666a9e92815b3ee297ffeeaabd4
DESKTOP-U7R5T0Q\luxuan <523957947@qq.com>
2018-07-03 22:30:58 +0800

    删除无用代码

--- lifesense/ConsoleLifesense/Program.cs
+++ lifesense/ConsoleLifesense/Program.cs
@@ -11,28 +11,9 @@ namespace ConsoleLifesense
 {
     class Program
     {
-        string APPid = "1b80cf89b99d608d0ee2845e1fae137d3aa3e04c";
-        string APPsecret = "ef1a5ab65c0c26747d85fc0832a9d5548e1c9cb7";
-        string Url = "";
         static void Main(string[] args)
         {
             new SyncDataManager().start();
-     
-            //String sha = getSHA();
-            //sha = "";
         }
-        private static String getSHA()
-        {
-            List<System.String> array = new List<System.String>();
-            array.Add("bd5cdb1a49ad26878a9ca800fdd53956c6b61882");
-            array.Add("818ea8c3572ec6277fe2bf0e8e58ffa274ca11a0");
-            array.Add("UKjPas12");
-            array.Add("code");
-            array.Add("1528937315037");
-            String[] arr = array.ToArray();
-            return SHAUtils.getSHACode(arr);
-        }
-
     }
-
 }
